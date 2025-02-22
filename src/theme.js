c4d41e6143e41939cb7a1ea93cbc5aed6963e544
Kaíque Vargas <kaiquerush@hotmail.com>
2025-02-22 12:11:59 -0300

    Added theme switching (light/dark)

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const themes = {
+    light: {
+      backgroundColor: '#ffffff',
+      textColor: '#000000',
+    },
+    dark: {
+      backgroundColor: '#000000',
+      textColor: '#ffffff',
+    },
+  };
+  
