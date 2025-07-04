cc039f0f7f46aa1cb4335220fc4c49ea8a40f998
Adriano Poier <adrianopoier@gmail.com>
2025-07-04 16:08:31 -0300

    Adiciona imagem e 4 personalizações aos cards, além de tornar as propriedes opcionais. Os cards ainda carecem de elementos iterativos e deveriam ser configurados para usarem cores do tema.

--- src/components/specsCard/SpecsCard.vue
+++ src/components/specsCard/SpecsCard.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
-defineProps<{
-    item: Record<string, unknown>
-    titleKey: string
-    subTitleKey: string
-    descriptionKey: string
-}>()
+withDefaults(
+    defineProps<{
+        item: Record<string, unknown>
+        imgSrcKey?: string
+        imgAltKey?: string
+        titleKey?: string
+        subTitleKey?: string
+        descriptionKey?: string
+        widith?: string
+        round?: string
+    }>(), {
+    imgSrcKey: '',
+    imgAltKey: '',
+    titleKey: '',
+    subTitleKey: '',
+    descriptionKey: '',
+    widith: '20em',
+    round: '1em',
+})
 </script>
 
 <template>
     <div class="card specs-card">
-        <div class="card-body">
-            <!-- <img src="..." class="card-img-top" alt="..."> -->
-            <h5 class="card-title">{{ item[titleKey] }}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{ item[subTitleKey] }}</h6>
-            <p class="card-text">{{ item[descriptionKey] }}</p>
+        <img v-if="imgSrcKey.length != 0" :src="item[imgSrcKey] as string" class="card-img-top specs-card-img"
+            :alt="item[imgAltKey] as string">
+        <div class="card-body specs-card-body">
+            <h5 v-if="titleKey.length != 0" class="card-title">{{ item[titleKey] }}</h5>
+            <h6 v-if="subTitleKey.length != 0" class="card-subtitle mb-2 text-muted">{{ item[subTitleKey] }}</h6>
+            <p v-if="descriptionKey.length != 0" class="card-text">{{ item[descriptionKey] }}</p>
             <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
         </div>
     </div>
@@ -21,9 +35,16 @@ defineProps<{
 
 <style scoped>
 .specs-card {
-    width: 20em;
+    width: v-bind(widith);
     background-color: darkslategray;
-    padding: 1em;
-    border-radius: 1em;
+    border-radius: v-bind(round);
+}
+
+.specs-card-body {
+    padding: v-bind(round);
+}
+
+.specs-card-img {
+    border-radius: v-bind(round) v-bind(round) 0em 0em;
 }
 </style>
--- src/components/specsCard/SpecsCardGrid.vue
+++ src/components/specsCard/SpecsCardGrid.vue
@@ -2,12 +2,30 @@
 import { ref, onMounted } from 'vue'
 import SpecsCard from './SpecsCard.vue'
 
-const props = defineProps<{
-    apiURL: string
-    titleKey: string
-    subTitleKey: string
-    descriptionKey: string
-}>()
+const props = withDefaults(
+    defineProps<{
+        apiURL: string
+        imgSrcKey?: string
+        imgAltKey?: string
+        titleKey?: string
+        subTitleKey?: string
+        descriptionKey?: string
+        cardWidith?: string
+        cardRound?: string
+        gap?: string
+        margin?: string
+    }>(), {
+    imgSrcKey: '',
+    imgAltKey: '',
+    titleKey: '',
+    subTitleKey: '',
+    descriptionKey: '',
+    cardWidith: '20em',
+    cardRound: '1em',
+    gap: '1em',
+    margin: '1em',
+    
+})
 
 const specs = ref<Record<string, unknown>[]>([])
 
@@ -19,8 +37,9 @@ onMounted(async () => {
 
 <template>
     <div class="specs-card-grid">
-        <SpecsCard v-for="(item, index) in specs" :key="index" :item="item" :title-key="titleKey"
-            :sub-title-key="subTitleKey" :description-key="descriptionKey" />
+        <SpecsCard v-for="(item, index) in specs" :key="index" :item="item" :img-src-key="imgSrcKey"
+            :img-alt-key="imgAltKey" :title-key="titleKey" :sub-title-key="subTitleKey"
+            :description-key="descriptionKey" :widith="cardWidith" :round="cardRound"/>
     </div>
 </template>
 
@@ -30,7 +49,7 @@ onMounted(async () => {
     flex-wrap: wrap;
     width: 100%;
     justify-content: center;
-    margin: 1em;
-    gap: 1em;
+    margin: v-bind(gap);
+    gap: v-bind(margin);
 }
 </style>
