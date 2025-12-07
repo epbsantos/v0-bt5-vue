d289e7e0a402ba85b9e785edf57f9c7ee56f1d0e
Larissa Steche <SEUEMAIL@gmail.com>
2025-12-07 19:12:26 -0300

    entrega do trabalho - versão Larissa

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 import { useI18n } from "vue-i18n";
 import Navbar from "@/components/NavBar.vue";
 import FooterBar from "@/components/FooterBar.vue";
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
 const { t } = useI18n();
 const route = useRoute();
 
 const useMainLayout = computed(() => route.name !== 'Tasks');
+
+const contador = ref(0);
 </script>
 
 <template>
@@ -20,6 +22,18 @@ const useMainLayout = computed(() => route.name !== 'Tasks');
     
     <div class="flex-fill d-flex flex-column">
       <main class="flex-fill" :class="{ 'container py-4': useMainLayout }">
+
+        <button 
+          @click="contador++" 
+          class="btn btn-primary mb-3"
+        >
+          Clique aqui ({{ contador }})
+        </button>
+
+        <p class="small text-secondary mb-3">
+  Rota atual: {{ route.name }}
+</p>
+
         <RouterView />
       </main>
     </div>
